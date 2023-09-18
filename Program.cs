6f70baf825fba6c80c59878b8b0348ab07202ae6
yazantahan <thn.soperma@gmail.com>
2023-09-18 18:19:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MP3Manager.NET7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
